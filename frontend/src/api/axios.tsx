32eae371632623645185433510ee449b5d2f2bdf
JohnKatoikos <gianniskatoikos1@gmail.com>
2025-07-15 13:00:43 +0300

    minor changes

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://ailademo.fly.dev", // Update with your backend URL
+    // baseURL: "https://ailademo.fly.dev", // Update with your backend URL
     // baseURL: "http://localhost:8080",
+    baseURL: '/',
     withCredentials: true
 })
 
