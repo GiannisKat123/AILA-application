8fca9160511d1cd125e1d3cab88d030e3096ecf0
JohnKatoikos <gianniskatoikos1@gmail.com>
2025-09-17 09:11:37 +0300

    Changes

--- new_docs/source/conf.py
+++ new_docs/source/conf.py
@@ -105,7 +105,7 @@ html_theme_options = {
     "navigation_depth": 4,
 }
 
-html_static_path = ["_static"]
+html_static_path = ["/source/_static"]
 
 autodoc_mock_imports = [
     "fastapi", "uvicorn", "sqlalchemy", "pydantic", "redis", "pymongo",
